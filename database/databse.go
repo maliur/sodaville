e6c91b96548efdf192d6a21c0b0fa5f407ef9f24
Maliur <fredrikanbacken@tutanota.com>
2020-05-05 15:07:48 +0200

    Add sql for insert and get command from db

--- database/databse.go
+++ database/databse.go
@@ -16,15 +16,50 @@ func NewDatabase(logger *log.Logger, conn *sql.DB) *Database {
 	return &Database{logger, conn}
 }
 
-func (db *Database) InsertCommand(name string) {
-	stmt, err := db.conn.Prepare(`INSERT INTO command(name) VALUES (?)`) // Prepare statement. This is good to avoid SQL injections
+func (db *Database) InsertCommand(name, response string, security bool) {
+	dbBool := 0
+	if security {
+		dbBool = 1
+	}
+
+	stmt, err := db.conn.Prepare(`INSERT INTO command(name, response, security) VALUES (?, ?, ?)`)
 	if err != nil {
 		db.logger.Fatalf("[SQL ERROR] could not prepare insert command: %v", err)
 	}
 
-	_, err = stmt.Exec(name)
+	_, err = stmt.Exec(name, response, dbBool)
 	if err != nil {
 		db.logger.Fatalf("[SQL ERROR] could not execute insert command: %v", err)
 	}
 	defer stmt.Close()
+}
+
+func (db *Database) GetCommandByName(name string) string {
+	var response string
+
+	stmt, err := db.conn.Prepare(`SELECT response FROM command WHERE name = ?`)
+	if err != nil {
+		db.logger.Fatalf("[SQL ERROR] could not prepare select command: %v", err)
+	}
+	defer stmt.Close()
+
+	rows, err := stmt.Query(name)
+	if err != nil {
+		db.logger.Fatalf("[SQL ERROR] could not execute select command: %v", err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		err := rows.Scan(&response)
+		if err != nil {
+			db.logger.Fatal(err)
+		}
+	}
+
+	err = rows.Err()
+	if err != nil {
+		db.logger.Fatal(err)
+	}
+
+	return response
 }
